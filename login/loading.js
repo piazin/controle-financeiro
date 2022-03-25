1e64fc986f5dd5314433da650e3c292651f01a9b
piazin <92053288+piazin@users.noreply.github.com>
2022-03-25 17:26:04 -0300

    update

--- login/loading.js
+++ login/loading.js
@@ -1,6 +1,5 @@
 function showLoading() {
     document.getElementById('loading').style.display = "flex";
-
 }
 
 function hideLoading() {
@@ -10,9 +9,24 @@ function hideLoading() {
 function userNot(error) {
     if(error.code == "auth/user-not-found") {
         document.getElementById('userNot').style.display = "block";
+        setTimeout(() => userRemoveNot(), 4000);
     } 
 
     if(error.code == "auth/wrong-password") {
         document.getElementById('userPass').style.display = "block";
+        setTimeout(() => userRemovePass(), 4000);
     }
+}
+
+function userRemoveNot() {
+    userRemove.userRemove().style.display = "none";
+}
+
+function userRemovePass() {
+    userRemove.userRmPs().style.display = "none";
+}
+
+const userRemove = {
+    userRmNot: () => document.getElementById('userNot'),
+    userRmPs: () => document.getElementById('userPass'),
 }
