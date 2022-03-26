51bfb4cc94fefbd0bbf456f47a67962f6d7bd241
piazin <92053288+piazin@users.noreply.github.com>
2022-03-26 14:23:48 -0300

    update

--- login/index.js
+++ login/index.js
@@ -125,3 +125,31 @@ const form = {
     passwordRequired: () => document.getElementById('password-required-error')
 }
 
+//teste google
+
+
+
+function loginGoogle() {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth()
+  .signInWithRedirect(provider)
+  .then((result) => {
+    /** @type {firebase.auth.OAuthCredential} */
+    var credential = result.credential;
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+    // ...
+  });
+}
+
